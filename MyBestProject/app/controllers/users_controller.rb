97ab814178a40e683bdc5f0fcb7df6e6a4fd223f
Denis Grebenyuk <dngrebenyuk@gmail.com>
2021-11-10 20:59:47 +0200

    add some renders

--- MyBestProject/app/controllers/users_controller.rb
+++ MyBestProject/app/controllers/users_controller.rb
@@ -1,10 +1,26 @@
 class UsersController < ApplicationController
+  skip_before_action :verify_authenticity_token
+  before_action :set_user, only: %i[show edit]
+  # layout false
+
   def index
     @users = User.all
+
+    respond_to do |format|
+      format.html { render layout: false }
+      format.json { render json: @users }
+    end
   end
 
-  def show
-    @user = User.find params[:id]
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to action: :index
+    else
+      # head :unprocessable_entity
+      render json: @user.errors
+    end
   end
 
   def search
@@ -14,5 +30,15 @@ class UsersController < ApplicationController
 
   def preview
 
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :age, profile_attributes: [])
+  end
+
+  def set_user
+    @user = User.find params[:id]
   end
 end
